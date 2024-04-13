700c487897c1342990dba45671904975718eac85
Mrinal Gupta <100479509+mrinalgupta24@users.noreply.github.com>
2024-04-13 21:18:15 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,294 @@
+export const Bio = {
+  name: "Mrinal Gupta",
+  roles: ["Full Stack Developer", "Android Developer", "Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/mrinalgupta24",
+  // resume:
+  //   "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/mrinal-gupta-469915236/",
+  twitter: "https://twitter.com/MrinalGupta24",
+  insta: "https://www.instagram.com/mrinalgupta24/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/favicon.ico",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://adobexdplatform.com/favicon.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://static.figma.com/app/icon/1/icon-192.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://suvidhafoundationedutech.org/img/favicon.jpg",
+    role: "Web Developer Intern",
+    company: "Suvidha Foundation",
+    date: "June 2022 - July 2022",
+    desc: "Worked as a lead developer, managing and maintaining a fully functional and responsive website with my team.",
+    skills: ["HTML", "CSS", "Javascript"],
+    //   doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.jimsrohini.org/assets/images/JIMS-logo-TC.jpg",
+    college: "Jagan Institute of Management Studies (JIMS), Rohini, New Delhi",
+    date: "Sept 2023 - Present",
+    grade: "8.5 CGPA",
+    desc: "I am currently pursuing a Master's degree in Computer Application at Jagan Institute of Management Studies, Rohini, New Delhi. I have taken courses in  OOPS, Operating Systems, DSA, DBMS, Computer Networks, Python, among others.",
+    degree: "MCA, Master of Computer Application",
+  },
+  {
+    id: 1,
+    img: "https://jimsd.org/assets/img/logo/logo.png",
+    college:
+      "Jagannath Institute of Management Studies (JIMS), Vasant Kunj, New Delhi",
+    date: "Jul 2020 - Jul 2023",
+    grade: "9.4 CGPA",
+    desc: "I completed my Bachelor's degree in Computer Application at Jagannath Institute of Management Studies , Vasant Kunj, New Delhi, where I have taken courses in  OOPS, Operating Systems, DSA, DBMS, Computer Networks, Python, among others.",
+    degree: "BCA, Bachelor of Computer Applications",
+  },
+  {
+    id: 2,
+    img: "https://greenfieldsschool.in/front_assets/themes/default/img/logo.png",
+    school: "Green Fields School, New Delhi",
+    date: "Apr 2019 - Mar 2020",
+    grade: "82%",
+    desc: "I completed my class 12 education at Green Fields School, New Delhi, where I studied Commerce with Computer Application.",
+    degree: "CBSE(XII), Commerce with Computer",
+  },
+  {
+    id: 3,
+    img: "https://greenfieldsschool.in/front_assets/themes/default/img/logo.png",
+    school: "Green Fields School, New Delhi",
+    date: "Apr 2017 - Mar 2018",
+    grade: "76%",
+    desc: "I completed my class 10 education at Green Fields School, New Delhi.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "NFT Marketplace",
+    date: "Mar 2024 - April 2024",
+    description:
+      "The project revolves around a marketplace where transactions occur via NFTs (non-fungible tokens) in a decentralized manner, eliminating intermediaries between buyers and sellers. Each NFT has a digital record of its ownership and transaction history. Your role involved managing a team of four members. The technology stack includes ReactJs, NextJs, Solidity, NodeJs, Ethereum, and Web3.js",
+    image:
+      "https://raw.githubusercontent.com/mrinalgupta24/NFT-Marketplace/main/nftmarkplace.png",
+    tags: ["React Js", "Next Js", "MongoDb", "Node Js", "Redux", "Solidity", "Web3.js"],
+    category: "web app",
+    github: "https://github.com/mrinalgupta24/NFT-Marketplace",
+    webapp: "https://nft-marktplace.vercel.app/",
+    member: [
+      {
+        name: "Mrinal Gupta",
+        img: "https://avatars.githubusercontent.com/u/100479509?v=4",
+        linkedin: "https://www.linkedin.com/in/mrinal-gupta-469915236/",
+        github: "https://github.com/mrinalgupta24",
+      },
+      {
+        name: "Shubham Aggarwal",
+        img: "https://avatars.githubusercontent.com/u/91524314?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/GJG-GUNDO",
+      },
+    ],
+  },
+  {
+    id: 0,
+    title: "Monumatic",
+    date: "Mar 2023 - May 2023",
+    description:
+      "The project aims to consolidate essential information about monuments and heritage sites in one place. Additionally, visitors will receive a QR-based E-Ticket, facilitating entry and exit at the monument. Your role involved leading a team of two members, and the technology stack includes HTML, CSS, JavaScript, PHP, and MySQL.",
+    image:
+      "https://github.com/mrinalgupta24/Monumatic-Web-App/blob/main/monumatic.png?raw=true",
+    tags: ["HTML", "CSS", "Javascript", "PHP", "MySQL"],
+    category: "web app",
+    github: "https://github.com/mrinalgupta24/Monumatic-Web-App",
+    webapp: "https://monumatic.tech/",
+    member: [
+        {
+          name: "Mrinal Gupta",
+          img: "https://avatars.githubusercontent.com/u/100479509?v=4",
+          linkedin: "https://www.linkedin.com/in/mrinal-gupta-469915236/",
+          github: "https://github.com/mrinalgupta24",
+        },
+        {
+          name: "Shubham Aggarwal",
+          img: "https://avatars.githubusercontent.com/u/91524314?v=4",
+          linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+          github: "https://github.com/GJG-GUNDO",
+        },
+      ],
+  },
+  {
+    id: 1,
+    title: "Suvidha Foundation",
+    date: "Jun 2022 - Jul 2022",
+    description:
+      "Developed a fully functional and responsive website for Suvidha Foundation. The technology stack for this project includes HTML, CSS, and JavaScript",
+    image:
+      "https://github.com/mrinalgupta24/SuvidhaFoundation/blob/main/SUVIDHA.png?raw=true",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+    ],
+    category: "web app",
+    github: "https://github.com/mrinalgupta24/SuvidhaFoundation",
+    webapp: "https://mrinalgupta24.github.io/SuvidhaFoundation/",
+  },
+//   {
+//     id: 2,
+//     title: "Knowledge Bazaar",
+//     date: "Jun 2022 - Aug 2022",
+//     description:
+//       "Developed an Online Purchasing and Selling Books App using Kotlin and Firebase",
+//     image:
+//       "https://github.com/mrinalgupta24/KnowledgeBazaar/blob/main/knowledgebazaar.jpg?raw=true",
+//     tags: ["Kotlin", "Firebase"],
+//     category: "machine learning",
+//     github: "https://github.com/mrinalgupta24/KnowledgeBazaar",
+//     webapp: "https://github.com/mrinalgupta24/KnowledgeBazaar",
+//   }
+];
+
+export const TimeLineData = [
+  { year: 2020, text: "Started my journey" },
+  { year: 2021, text: "Worked as a web developer" },
+  { year: 2022-23, text: "Did Web Dev Mastery" },
+  { year: 2024, text: "Shared my projects with the world" },
+];
